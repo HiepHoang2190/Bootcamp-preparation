07bac1555248f3df3adc1ec8e19297fa3ac4e4e2
unknown <hoangbachhiep2190@gmail.com>
2020-03-05 17:27:02 +0700

    update

--- exercises/bienvatoantu/baitap2.js
+++ exercises/bienvatoantu/baitap2.js
@@ -1,8 +1,9 @@
 function trungbinh (){
-let toan = parseInt(document.getElementById("toan").value);
-let hoa = parseInt(document.getElementById("hoa").value);
-let sinh = parseInt(document.getElementById("sinh").value);
-let vatly = parseInt(document.getElementById("vatly").value);
+
+let toan = parseFloat(document.getElementById("toan").value);
+let hoa = parseFloat(document.getElementById("hoa").value);
+let sinh = parseFloat(document.getElementById("sinh").value);
+let vatly = parseFloat(document.getElementById("vatly").value);
 let area = (toan + vatly + hoa + sinh)/4;
 let tong = (toan + vatly + hoa + sinh);
 document.getElementById("aaaa").innerHTML =  "Điểm trung bình: " + area;
@@ -10,7 +11,7 @@ document.getElementById("bbb").innerHTML =  "Tổng " + tong;
 }
 
 function change(){
-  let  inputC = parseInt(document.getElementById("inputC").value);
+  let  inputC = parseFloat(document.getElementById("inputC").value);
   let F = (inputC *9/5)+32;
   document.getElementById("ccc").innerHTML =  F;
 }
@@ -18,14 +19,14 @@ function change(){
 
 
 function Radius(){
-    let  inputR = parseInt(document.getElementById("inputR").value);
-    let R = (inputR *inputR)*3.14;
+    let  inputR = parseFloat(document.getElementById("inputR").value);
+    let R = (inputR *inputR)*Math.PI;
     document.getElementById("ddd").innerHTML =  R;
   }
 
 
   function chuvi(){
-    let  inputZ = parseInt(document.getElementById("inputZ").value);
-    let Z = (inputZ *2)*3.14;
+    let  inputZ = parseFloat(document.getElementById("inputZ").value);
+    let Z = (inputZ *2)*Math.PI;
     document.getElementById("eee").innerHTML =  Z;
   }
